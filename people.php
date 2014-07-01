a5b21740b7687ac4ab2088d6a7e388167e1bda24
Binny V A <binnyva@gmail.com>
2014-07-01 10:48:35 +0530

    Fixed a bug in people.php

--- people.php
+++ people.php
@@ -6,7 +6,7 @@ if(!empty($QUERY['vertical_id'])) $vertical = "AND G.vertical_id=$QUERY[vertical
 $people = $sql->getById("SELECT U.id,U.name FROM User U
 		INNER JOIN UserGroup UG ON U.id=UG.user_id 
 		INNER JOIN `Group` G ON G.id=UG.group_id
-		WHERE (G.type='fellow' OR G.type='national') $vertical AND U.city_id=$QUERY[city_id] AND U.status='1'
+		WHERE (G.type='fellow' OR G.type='national') $vertical AND U.city_id=$QUERY[city_id] AND U.status='1' AND U.user_type='volunteer'
 		ORDER BY U.name");
 $grades = $sql->from("OKR_Grade")->select(array("user_id", "grade"))->where('user_id != 0')->get('byid');
 
